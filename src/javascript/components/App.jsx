a4324c8d7ed08424433cf85f0aca24ee4e7cd7e7
Ralph Effting <ralfting@gmail.com>
2017-12-17 18:36:26 -0200

    Create components folder and move all component to inside him

--- src/javascript/components/App.jsx
+++ src/javascript/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import history from '../history';
+
 import TopBar from './TopBar';
 
 export default function App() {
   return (
-    <div>
+    <Router history={history}>
       <TopBar title="References Project" />
-    </div>
+    </Router>
   );
 }
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router-dom';
 
-import history from './history';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
-  <Router history={history}>
-    <Route exact path="/" component={App} />
-  </Router>,
+  <App />,
   document.getElementById('root'),
 );
